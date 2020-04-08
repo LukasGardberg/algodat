75907609e7488900a10e7285f708ef4a12a00ecc
DeeFly4 <daniel.holm@majodang.se>
2020-04-08 16:48:57 +0200

    Gives answers for individual files but doesn't work when running check_solution script

--- EDAF05-labs-public-master/2wordladders/solver.py
+++ EDAF05-labs-public-master/2wordladders/solver.py
@@ -5,46 +5,59 @@ line1 = sys.stdin.readline().split()
 N = int(line1[0])
 Q = int(line1[1])
 
-
-# Def node class
-class Node:
-
-    def __init__(self, word):
-        self.neighbors = []
-        self.word = word
-
-    def add_neighbor(self, node):
-        self.neighbors.append(node)
-
-
 # Read in words and store in nodes
-words = [Node(sys.stdin.readline().strip()) for _ in range(N)]
+word_map = {}
 
+for i in range(N):
+    word_map[sys.stdin.readline().strip()] = []
 
-def connected(node1, node2):
-    # returns if node1 and node2 should be connected
-    string1 = node1.word[1:]
-    string2 = node2.word
+def connected(word1, word2):
+    # returns true if word1 has an edge to word2
 
-    for l in string1:
-        if l in string2:
-            string2 = string2.replace(l, '', 1)
+    for char in word1[1:]:
+        if char in word2:
+            word2 = word2.replace(char, '', 1)
         else:
             return False
 
     return True
 
 
-for word in words:
-    temp_node = Node(word)
+for word1 in word_map:
+    for word2 in word_map:
+        if word1 != word2 and connected(word1, word2):
+            word_map[word1].append(word2)
 
 
 def bfs(start, stop):
-    visited = set()
+    visited = {start}
+    q = [start]
+    parents = {}
+    path_length = 1
 
+    while q:
+        v = q.pop(0)
+        for neighbor in word_map[v]:
+            # if neighbor has not been visited then add it to visited and append neighbor to q
+            if neighbor not in visited:
+                visited.add(neighbor)
+                q.append(neighbor)
+                parents[neighbor] = v
+                if neighbor == stop:
+                    temp_neighbor = stop
 
+                    while parents[temp_neighbor] != start:
+                        temp_neighbor = parents[temp_neighbor]
+                        path_length += 1
 
+                    return path_length
 
-    pass
+    return 'Impossible'
 
+for _ in range(Q):
+    line = sys.stdin.readline().split()
+    start = line[0]
+    stop = line[1]
+
+    print(bfs(start, stop))
 
