eb13a4957887c70b2d18206030113d2978b7d9e2
LukasGardberg <lukasgardberg2@gmail.com>
2020-05-12 15:08:55 +0200

    bad working solution

--- EDAF05-labs-public-master/3makingfriends/solver.py
+++ EDAF05-labs-public-master/3makingfriends/solver.py
@@ -3,7 +3,6 @@ import heapq
 
 line1 = sys.stdin.readline().split()
 
-
 N = int(line1[0])  # Nbr of people
 M = int(line1[1])  # Nbr of edges
 
@@ -13,9 +12,36 @@ for _ in range(M):
     line = sys.stdin.readline().split()
     dists[int(line[0]) - 1][int(line[1]) - 1] = dists[int(line[1]) - 1][int(line[0]) - 1] = int(line[2])
 
-q = [(float('inf'), None) for _ in range(N - 1)]
+root = 0
+total_dist = 0
+
+mst = set()
+visited = {root}
+
+Q = []
+
+# Add distances and edges from root to heapq
+for i in range(N):
+    d = dists[root][i]
+    if d != -1:
+        Q.append((d, root, i))
+
+heapq.heapify(Q)
+
+while Q:
+    # Pop nearest vertex from our MST
+    dist, frm, to = heapq.heappop(Q)
+
+    if to not in visited:
+        visited.add(to)
+
+        total_dist += dist
+
+        # Check next edges to consider
+        for i in range(N):
+            d_temp = dists[to][i]
+            if i not in visited and d_temp != -1:
+                heapq.heappush(Q, (d_temp, to, i))
 
 
-heapq.heapify(q)
-print(heapq.heappop(q))
-print(q)
+print(total_dist)
