52b4d3c6610e19d4640b457b1b03c281a3826cf3
LukasGardberg <lukasgardberg2@gmail.com>
2020-05-13 12:14:41 +0200

    faster working solution, no memory error

--- EDAF05-labs-public-master/3makingfriends/solver.py
+++ EDAF05-labs-public-master/3makingfriends/solver.py
@@ -1,6 +1,7 @@
 import sys
 import heapq
 import time
+from collections import defaultdict
 
 t_read = time.time()
 
@@ -9,29 +10,29 @@ line1 = sys.stdin.readline().split()
 N = int(line1[0])  # Nbr of people
 M = int(line1[1])  # Nbr of edges
 
-dists = [[-1 for _ in range(N)] for _ in range(N)]
+# test_map = {}
+dists = defaultdict(list)
 
+# Store adjacent nodes and edge weights in map
 for _ in range(M):
     line = sys.stdin.readline().split()
-    dists[int(line[0]) - 1][int(line[1]) - 1] = dists[int(line[1]) - 1][int(line[0]) - 1] = int(line[2])
+    dists[int(line[0])].append((int(line[2]), int(line[1])))
+    dists[int(line[1])].append((int(line[2]), int(line[0])))
 
 t_read = time.time() - t_read
 
 t_makeheap = time.time()
 
-root = 0
+root = 1
 total_dist = 0
 
-mst = set()
 visited = {root}
 
 Q = []
 
 # Add distances and edges from root to heapq
-for i in range(N):
-    d = dists[root][i]
-    if d != -1:
-        Q.append((d, root, i))
+for dist, to in dists[root]:
+    Q.append((dist, root, to))
 
 heapq.heapify(Q)
 
@@ -49,10 +50,10 @@ while Q:
         total_dist += dist
 
         # Check next edges to consider
-        for i in range(N):
-            d_temp = dists[to][i]
-            if i not in visited and d_temp != -1:
-                heapq.heappush(Q, (d_temp, to, i))
+        for next_d, next_to in dists[to]:
+            if next_to not in visited:
+                heapq.heappush(Q, (next_d, to, next_to))
+
 
 t_solve = time.time() - t_solve
 
