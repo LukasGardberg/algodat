08f65f57b81dcc2ab05a1c487bf56b9849b041e1
LukasGardberg <lukasgardberg2@gmail.com>
2020-05-22 10:32:39 +0200

    working solution

--- EDAF05-labs-public-master/5gorilla/solver.py
+++ EDAF05-labs-public-master/5gorilla/solver.py
@@ -16,7 +16,7 @@ def optimal_alignment(string1, string2):
     n = len(string1)
     m = len(string2)
 
-    # contains the optimal alignment score for substrings s1[:i], s2[:j] in A[i,j]
+    # contains the optimal alignment score for substrings s1[:row], s2[:col] in A[row,col]
     substr_score = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
     # add scores for just inserting gaps
@@ -26,19 +26,69 @@ def optimal_alignment(string1, string2):
         substr_score[i][0] = gap_pen * i
 
     # calculate optimal alignment scores
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
+    for row in range(1, m + 1):
+        for col in range(1, n + 1):
             # get score for matching both letters
-            match = substr_score[i-1][j-1] + cost[letter_index[string1[j-1]]][letter_index[string2[i-1]]]
+            match = substr_score[row-1][col-1] + cost[letter_index[string1[col-1]]][letter_index[string2[row-1]]]
 
-            # score for '*' in string 1
-            gap_s1 = substr_score[i][j-1] + gap_pen
+            # match opposing letter with '*' ('*' in string1)
+            gap_s1 = substr_score[row][col-1] + gap_pen
 
-            # score for '*' in string 2
-            gap_s2 = substr_score[i-1][j] + gap_pen
+            # match this letter with '*' ('*' in string2)
+            gap_s2 = substr_score[row-1][col] + gap_pen
 
-            substr_score[i][j] = max(match, gap_s1, gap_s2)
+            substr_score[row][col] = max(match, gap_s1, gap_s2)
 
-    print(substr_score)
+    # Backtrack and find correct optimal string alignment
 
-optimal_alignment('ABA', 'ACA')
+    row = m
+    col = n
+
+    opt_string1 = ''
+    opt_string2 = ''
+
+    while row > 0 and col > 0:
+
+        # if our score corresponds to matching both letters, add these to optimal strings
+        if substr_score[row][col] == substr_score[row-1][col-1] + \
+                cost[letter_index[string1[col-1]]][letter_index[string2[row-1]]]:
+            opt_string1 = string1[col - 1] + opt_string1
+            opt_string2 = string2[row - 1] + opt_string2
+
+            row -= 1
+            col -= 1
+        # else if score corresponds to matching letter in string1 with '*' in string2, add to opt string
+        elif substr_score[row][col] == substr_score[row][col-1] + gap_pen:
+            opt_string1 = string1[col - 1] + opt_string1
+            opt_string2 = '*' + opt_string2
+
+            col -= 1
+        # else (always) if score corresponds to matching letter in string 2 with '*' in string1, add to opt string
+        elif substr_score[row][col] == substr_score[row-1][col] + gap_pen:
+            opt_string1 = '*' + opt_string1
+            opt_string2 = string2[row - 1] + opt_string2
+
+            row -= 1
+        else:
+            print('somethings fucky')
+
+    # Check if one of the strings are shorter than the other, and therefore needs spaces to align
+    if row > 0:
+        while row > 0:
+            opt_string1 = '*' + opt_string1
+            opt_string2 = string2[row - 1] + opt_string2
+            row -= 1
+    elif col > 0:
+        while col > 0:
+            opt_string2 = '*' + opt_string2
+            opt_string1 = string1[col - 1] + opt_string1
+            col -= 1
+
+    return opt_string1, opt_string2
+
+
+# read in queries and print
+for _ in range(n_queries):
+    line = sys.stdin.readline().split()
+    str1, str2 = optimal_alignment(line[0], line[1])
+    print(str1, str2)
